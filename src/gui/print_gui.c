b3d8bf6f276acfac43279ced4e967a2749fef1f7
anatole zeyen <anatole.zeyen@epitech.eu>
2017-04-11 14:57:57 +0200

    add

--- src/gui/print_gui.c
+++ src/gui/print_gui.c
@@ -5,14 +5,14 @@
 ** Login   <anatole.zeyen@epitech.net>
 ** 
 ** Started on  Tue Apr 11 14:03:40 2017 anatole zeyen
-** Last update Tue Apr 11 14:21:36 2017 anatole zeyen
+** Last update Tue Apr 11 14:57:37 2017 anatole zeyen
 */
 
 #include "raytracer.h"
 
 int	print_gui(t_screen *screen)
 {
-  sfVector2i localPoisiton;
+  sfVector2i localPosition;
 
-  localPosition = sfMouse_getPosition(screen->w);
+  localPosition = sfMouse_getPosition(screen->win);
 }
