6e67d6078fc00f0190ef6a43546fdc30c250ac7a
nathan.schwarzepitech.eu <nathan.schwarzepitech.eu@epitech.eu>
2017-05-20 20:16:23 +0200

    removed shadow bug on collision

--- src/tracing/shadow.c
+++ src/tracing/shadow.c
@@ -5,19 +5,30 @@
 ** Login   <anatole.zeyen@epitech.net>
 **
 ** Started on  Tue May 16 11:39:16 2017 anatole zeyen
-** Last update Fri May 19 13:35:46 2017 anatole zeyen
+** Last update Sat May 20 20:06:46 2017 schwarzy
 */
 
 #include "raytracer.h"
 
-int	compare_current_obj_and_other(t_object *current_obj, t_object other)
+int	not_this_obj(t_object *current_obj, t_object other)
 {
-  if (current_obj->type != other.type)
-    return (1);
-  if (current_obj->position.x == other.position.x && current_obj->position.y
-      == other.position.y &&
-      current_obj->position.z == other.position.z && current_obj->value ==
-      other.value)
+  if (current_obj->position.x == other.position.x
+      && current_obj->position.y == other.position.y
+      && current_obj->position.z == other.position.z
+      && current_obj->value == other.value
+      && current_obj->type == other.type)
+    return (0);
+  return (1);
+}
+
+int		is_shadowing(t_object *shadower, t_object *shadowed,
+			     t_light *light)
+{
+  if (shadower->position.z - shadower->value < shadowed->position.z
+      && shadower->position.z < light->coordinates.z)
+    return (0);
+  if (shadower->position.z + shadower->value > shadowed->position.z
+      && shadowed->position.z > light->coordinates.z)
     return (0);
   return (1);
 }
@@ -25,32 +36,27 @@ int	compare_current_obj_and_other(t_object *current_obj, t_object other)
 int			shadow(sfVector3f light_v, t_screen *screen,
 			       t_object *current_obj, sfVector3f *inter_point)
 {
-  unsigned int		x;
   float			closest;
   t_nodeObject		*temp;
   sfVector3f		dir_vector;
   sfVector3f		obj_pos;
 
-  x = 0;
   temp = screen->objects.begin;
   closest = -1.0;
   dir_vector.x = -light_v.x;
   dir_vector.y = -light_v.y;
   dir_vector.z = -light_v.z;
-  while (x < screen->objects.count)
+  while (temp != NULL)
     {
       obj_pos.x = temp->object.position.x - inter_point->x;
       obj_pos.y = temp->object.position.y - inter_point->y;
       obj_pos.z = temp->object.position.z - inter_point->z;
-      closest = temp->object.intersect(&dir_vector, &obj_pos, &temp->
-				       object.position, temp->object.value);
-      if ((compare_current_obj_and_other(current_obj, temp->object)) != 0 &&
-	  (closest > 0.0f) &&
-	  ((current_obj->position.z + current_obj->value <
-	    screen->lights->coordinates.z) || (temp->object.position.z / 2>
-					       screen->lights->coordinates.z)))
+      closest =
+	temp->object.intersect(&dir_vector, &obj_pos,
+			       &current_obj->position, temp->object.value);
+      if (not_this_obj(current_obj, temp->object) && closest >= 0.0 &&
+	  is_shadowing(current_obj, &temp->object, screen->lights))
 	return (0);
-      x++;
       temp = temp->next;
     }
   return (1);
