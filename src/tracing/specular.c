0bea6b876678c1e84ca3ef5dcfaa85d0a9975a71
anatole zeyen <anatole.zeyen@epitech.eu>
2017-05-22 16:24:36 +0200

    add

--- src/tracing/specular.c
+++ src/tracing/specular.c
@@ -1,29 +1,32 @@
 /*
 ** specular.c for  in /home/anatole.zeyen/raytracer2/src/tracing
-** 
+**
 ** Made by anatole zeyen
 ** Login   <anatole.zeyen@epitech.net>
-** 
+**
 ** Started on  Mon May 22 15:52:27 2017 anatole zeyen
-** Last update Mon May 22 16:07:15 2017 anatole zeyen
+** Last update Mon May 22 16:24:11 2017 anatole zeyen
 */
 
 sfVector3f	vector_reflect(sfVector3f *normal_light,
 			       sfVector3f *normal_normal)
 {
   sfVector3f	reflect_vector;
+  float		angle;
 
-  reflect_vector.x = -2 * normal_normal.x * (normal_light.x * normal_normal.x)
+  angle = normal_light.x * normal_normal.x + normal_light.y *
+    normal_normal.y + normal_light.z * normal_normal.z;
+  reflect_vector.x = -2 * angle * (normal_light.x + normal_normal.x)
     + normal_light.x;
-  reflect_vector.y = -2 * normal_normal.y * (normal_light.y * normal_normal.y)
+  reflect_vector.y = -2 * angle * (normal_light.y * normal_normal.y)
     + normal_light.y;
-  reflect_vector.z = -2 * normal_normal.z * (normal_light.z * normal_normal.z)
+  reflect_vector.z = -2 * angle * (normal_light.z * normal_normal.z)
     + normal_light.z;
   return (reflect_vector);
 }
 
 float		specular(sfVector3f dir_vector, sfVector3f *light,
-			 float coef, sfVector3f *normal)
+			 float coef, sfVector3f *normal) // normal = la normale du pt d'intersection entre light_v et obj
 {
   float		angle;
   sfVector3f	normal_light;
@@ -37,7 +40,6 @@ float		specular(sfVector3f dir_vector, sfVector3f *light,
     normal_normal.y + dir_vector.z * normal_normal.z;
   if (angle <= 0 || coef == 0 || angle >= 1)
     return (0);
-  coef = 200;
-  angle = pow(angle, coef);
+  angle = pow(angle, 100);
   return (angle);
 }
