954971ca27f71204e637687947773ceb089996c9
nathan.schwarzepitech.eu <nathan.schwarzepitech.eu@epitech.eu>
2017-05-26 18:53:57 +0200

    improved diffuse white light

--- src/tracing/color.c
+++ src/tracing/color.c
@@ -5,7 +5,7 @@
 ** Login   <nathan.lebon@epitech.eu>
 **
 ** Started on  Sat Apr 29 16:14:21 2017 NANAA
-** Last update Fri May 26 18:44:45 2017 schwarzy
+** Last update Fri May 26 18:53:17 2017 schwarzy
 */
 
 # include <SFML/Graphics/Color.h>
@@ -14,10 +14,13 @@
 sfColor	diffuse_color(sfColor color, float cos, sfColor obj_color)
 {
   if (color.r == 255 && color.g == 255 && color.b == 255)
-    return (obj_color);
-  color.r = (color.r * cos) / 2 + (obj_color.r * cos) / 2;
-  color.g = (color.g * cos) / 2 + (obj_color.g * cos) / 2;
-  color.b = (color.b * cos) / 2 + (obj_color.b * cos) / 2;
+    {
+      change_color(&obj_color, cos);
+      return (obj_color);
+    }
+  color.r = (color.r + obj_color.r) * (cos / 2);
+  color.g = (color.g + obj_color.g) * (cos / 2);
+  color.b = (color.b + obj_color.b) * (cos / 2);
   return (color);
 }
 
